691f0da84e925eb0acaf2f3ed76398adbb40d7e0
LoC <loc@locrealloc.de>
2022-11-28 07:21:06 +0100

    Added a footer
    Signed-off-by: LoC <loc@locrealloc.de>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,14 @@
 	import "../app.css";
 </script>
 
-<div class="h-full dark:bg-slate-900">
+<div class="min-h-full dark:bg-slate-900">
 	<slot />
 </div>
+<footer class="fixed bottom-0 grid w-full bg-slate-800 object-bottom p-9 text-white">
+	<h3 class="text-center">
+		Diese Webseite ist freie und quelloffene Software! <a
+			class="text-blue-500 hover:underline"
+			href="https://github.com/LoCrealloc/abisite">Schau dir unseren Code an!</a
+		>
+	</h3>
+</footer>
