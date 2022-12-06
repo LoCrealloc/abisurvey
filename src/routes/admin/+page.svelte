02e0b2d74ddd65a89b42bfff57eb177f67602e46
LoC <loc@locrealloc.de>
2022-12-06 07:26:26 +0100

    Added admin landing page
    Signed-off-by: LoC <loc@locrealloc.de>

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,5 @@
+<div class="absolute flex h-full w-full items-center justify-center overflow-hidden">
+	<div class="rounded-xl bg-slate-800 p-20 text-center dark:bg-slate-500">
+		<h3 class="text-xl text-white">Willkommen!</h3>
+	</div>
+</div>
