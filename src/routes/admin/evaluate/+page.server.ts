8294a268e19dffa187def356b0cfcec48ffc3a27
LoC <loc@locrealloc.de>
2023-01-25 13:34:20 +0100

    Added server side evaluation ability

--- src/routes/admin/evaluate/+page.server.ts
+++ src/routes/admin/evaluate/+page.server.ts
@@ -1,8 +1,9 @@
 import type { PageServerLoad } from "./$types";
 import { Answer } from "$lib/server/models/answer";
 import { db } from "$lib/server/database";
-import { AnswerPossibility } from "../../../lib/server/models/answerpossibility";
-import { Question } from "../../../lib/server/models/question";
+import { AnswerPossibility } from "$lib/server/models/answerpossibility";
+import { Question } from "$lib/server/models/question";
+import { PairAnswer } from "$lib/server/models/pairanswer";
 
 export const load: PageServerLoad = async () => {
 	return {
@@ -11,9 +12,22 @@ export const load: PageServerLoad = async () => {
 				attributes: [
 					"questionId",
 					"answerPossibilityId",
-					[db.fn("count", "answerPossibilityIdA"), "count"],
+					[db.fn("count", "answerPossibilityId"), "count"],
 				],
 				group: ["questionId", "answerPossibilityId"],
+			})
+		).map((row) => {
+			return row.dataValues;
+		}),
+		pairanswers: (
+			await PairAnswer.findAll({
+				attributes: [
+					"questionId",
+					"answerOneId",
+					"answerTwoId",
+					[db.fn("count", "answerOneId"), "count"],
+				],
+				group: ["questionId", "answerOneId", "answerTwoId"],
 			})
 		).map((row) => {
 			return row.dataValues;
