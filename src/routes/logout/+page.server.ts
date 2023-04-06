bc6a7cabd2bcf23a3189f7e4380a4346eedea021
LoC <loc@locrealloc.de>
2023-04-06 14:04:54 +0200

    Added admin jwt removal for the logout button

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -4,6 +4,7 @@ import { redirect } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ cookies, locals }) => {
 	await cookies.delete("usersession");
+	await cookies.delete("token");
 
 	locals.userId = null;
 
